f83be6e7f8736a4d93dda34f7bf1284a147c56c2
Denis Davydov <follenlast2@gmail.com>
2023-06-24 10:14:11 +0300

    token bucket rate limit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"time"
+
+	mw "github.com/andyollylarkin/gofiber_ratelimit/middlewares"
+	"github.com/gofiber/fiber/v2"
+)
+
+func NotThrottledRoute(r *fiber.Ctx) error {
+	r.Accepts("application/json")
+
+	return r.SendString("Hello")
+}
+
+func ThrottledRoute(r *fiber.Ctx) error {
+	r.Accepts("application/json")
+
+	return r.SendString("Hello throttled")
+}
+
+func main() {
+	app := fiber.New()
+	mw := mw.ThrottleTokenBucketMw(10, time.Second*30)
+	nt := app.Group("/not_throttled")
+	t := app.Group("/throttled", mw)
+
+	nt.Get("/get", NotThrottledRoute)
+	t.Get("/get", ThrottledRoute)
+	app.Listen(":8080")
+}
